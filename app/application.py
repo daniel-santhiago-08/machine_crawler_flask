07adae83a07920c6401e5b15e89ae872dd0e4c26
Santhiago <daniel.santhiago@thrive-wmccann.com>
2020-05-07 13:39:10 -0300

    application Path fix

--- app/application.py
+++ app/application.py
@@ -1,11 +1,11 @@
 from app import create_app, db
 
-app = create_app()
+application = create_app()
 
 
-@app.before_first_request
+@application.before_first_request
 def cria_banco():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
